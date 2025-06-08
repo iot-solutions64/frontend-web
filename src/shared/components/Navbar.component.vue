56ece9efb78af675405cd42de9fd355e38f00a09
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-06-07 23:53:26 -0500

    style: updated the navbar styles

--- src/shared/components/Navbar.component.vue
+++ src/shared/components/Navbar.component.vue
@@ -34,88 +34,107 @@ const toggle = (event) => {
 </script>
 
 <template>
-  <header>
-    <nav class="nav-container" id="navbar">
-      <div class="nav-logo">
-        <img src="/assets/images/hydrosmart-logo.png" width="50px" height="50px" alt="software logo" />
-        <p>HydroSmart</p>
-      </div>
-      <div class="desktop-menu">
-        <ul class="nav-sections">
-          <li><router-link to="/crops">Cultivos</router-link></li>
-          <li><router-link to="/water">Gestión de agua</router-link></li>
-          <li><router-link to="/systems">Sistemas</router-link></li>
-        </ul>
-        <pv-button @click="store.dispatch('logout')"
-                   text
-                   icon="pi pi-sign-out"
-                   style="color: white; font-size: 20px;"
-                   aria-label="log out"/>
-      </div>
-      <div class="mobile-menu">
-        <pv-button icon="pi pi-bars"
-                   text
-                   style="color: white; font-size: 20px;"
-                   @click="toggle"/>
-        <pv-menu ref="menu"
-                 id="overlay_menu"
-                 :model="items"
-                 :popup="true"
-                 />
-      </div>
-    </nav>
-  </header>
+  <nav id="navbar">
+    <div class="nav-logo">
+      <img src="/assets/images/hydrosmart-logo.png" width="50px" height="50px" alt="HydroSmart Logo"/>
+      <h5>HydroSmart</h5>
+    </div>
+    <div class="desktop-menu">
+      <ul class="nav-links">
+        <li><router-link to="/crops">Cultivos</router-link></li>
+        <li><router-link to="/water">Gestión de agua</router-link></li>
+        <li><router-link to="/systems">Sistemas</router-link></li>
+      </ul>
+      <pv-button @click="store.dispatch('logout')"
+                 label="Salir"
+                 text
+                 icon="pi pi-sign-out"
+                 iconPos="right"
+                 aria-label="log out"/>
+    </div>
+    <div class="mobile-menu">
+      <pv-button icon="pi pi-bars"
+                 text
+                 style="color: white; font-size: 20px;"
+                 @click="toggle"/>
+      <pv-menu ref="menu"
+               id="overlay_menu"
+               :model="items"
+               :popup="true"
+      />
+    </div>
+  </nav>
 </template>
 
 <style scoped>
-.nav-container {
-  display: flex;
+nav{
   height: 75px;
+  display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px 20px;
+  padding: 0 1rem 0 1rem;
+  color: var(--text-color-inverted);
   background-color: var(--primary-color-dark);
 }
 
-.desktop-menu {
+.nav-logo {
+  width: 30%;
   display: flex;
   align-items: center;
+  font-weight: bold;
+}
+
+.nav-logo>img {
+  margin: 10px;
+}
+
+.desktop-menu {
+  width: 70%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: right;
 }
 
 .mobile-menu {
   display: none;
 }
 
-.nav-sections {
+.nav-links {
+  height: 100%;
   display: flex;
-  gap: 30px;
-  list-style: none;
   margin-right: 50px;
+  list-style: none;
 }
 
-.nav-sections li a {
-  text-decoration: none;
-  font-weight: 100;
-  font-size: 24px;
-  color: var(--background-color);
-}
-
-.nav-sections li:hover {
-  transform: scale(1.1);
-}
-
-.nav-logo {
+.nav-links>li {
+  height: 100%;
   display: flex;
   align-items: center;
+  transition: background-color 0.4s ease;
 }
 
-.nav-logo img {
-  margin: 10px;
-}
-
-.nav-logo p {
+.nav-links>li>a {
   font-size: 24px;
+  padding: 0 1rem 0 1rem;
+  text-decoration: none;
   color: var(--background-color);
+}
+
+.nav-links li:hover {
+  background-color: var(--primary-color-darker);
+}
+
+.p-button{
+  font-size: 20px;
+  background-color: transparent;
+  color: white;
+  transition: background-color 0.4s ease;
+}
+
+.p-button:hover {
+  background-color: red !important;
+  color: white !important;
 }
 
 @media (max-width: 768px) {
